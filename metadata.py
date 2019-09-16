d65f974f4676bf04ac59437f7ef823061ffc466d
ashlynns <ashlynn.steeves@icloud.com>
2019-09-16 12:17:19 -0600

    push incase i mess this up with what im about to try

--- metadata.py
+++ metadata.py
@@ -1,17 +1,30 @@
 import os 
+import Bio
+from Bio import SeqIO
 
 directories = (os.listdir("refseq/bacteria")) # list of all the folders in the bacteria folder
 internal_directories_two = [] # list of the files in each folder
 internal_directories_one = [] # list of the fna files
-i =0 
+fna_file_paths =[] # list of the paths to all the fna files
+i=0 
+j=0
 
 for file in directories:
-	location = "refseq/bacteria/"+file
-	internal_directories_two.append(os.listdir(location))
+	location_1 = "refseq/bacteria/"+file
+	internal_directories_two.append(os.listdir(location_1))
 
 while i< len(internal_directories_two):
 	internal_directories_one.append(internal_directories_two[i][0]) 
 	i=i+1
 
-print(internal_directories_one)
-print(len(internal_directories_one))
+while j <len(internal_directories_two): 
+	location_2 = "refseq/bacteria/"+directories[j]+"/"+internal_directories_one[j]
+	fna_file_paths.append(location_2)
+	j=j+1
+	for seq_record in SeqIO.parse(location_2, "fasta"):
+		print(seq_record.id)
+		print(repr(seq_record.seq))
+		print(len(seq_record))
+		print("next")
+
+
