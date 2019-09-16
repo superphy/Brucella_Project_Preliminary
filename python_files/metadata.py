f3f913532b73cadbea98147f556044ec9e117393
ashlynns <ashlynn.steeves@icloud.com>
2019-09-16 15:12:53 -0600

    putting all the python files in one place

---
+++ python_files/metadata.py
@@ -0,0 +1,29 @@
+import os 
+import gzip
+
+directories = (os.listdir("refseq/bacteria")) # list of all the folders in the bacteria folder
+internal_directories_two = [] # list of the files in each folder
+internal_directories_one = [] # list of the fna.gz files
+i=0 
+j=0
+
+for file in directories:
+	location_1 = "refseq/bacteria/"+file
+	internal_directories_two.append(os.listdir(location_1))
+
+while i< len(internal_directories_two):
+	first_entry = internal_directories_two[i][0]
+	if first_entry == "MD5SUMS":
+		internal_directories_one.append(internal_directories_two[i][1])
+	else:
+		internal_directories_one.append(first_entry)
+	i=i+1
+
+while j <len(internal_directories_two): 
+	location_2 = "refseq/bacteria/"+directories[j]+"/"+internal_directories_one[j]
+	argument = "gunzip " + location_2
+	os.system(argument)
+	print(j)
+	j=j+1
+
+print("done")
