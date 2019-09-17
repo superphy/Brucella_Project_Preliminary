80614fbf94b2c689d1391436f62baf5a7f6b6571
ashlynns <ashlynn.steeves@icloud.com>
2019-09-17 10:50:56 -0600

    used to parse and compile brucella metadata from ncbi downloads

--- python_files/metadata.py
+++ python_files/metadata.py
@@ -1,13 +1,14 @@
 import os 
-import gzip
-import Bio 
 from Bio import SeqIO
+import re 
+import csv
 
 directories = (os.listdir("../refseq/bacteria")) # list of all the folders in the bacteria folder
 internal_directories = [] # list of the files in each folder
 internal_directories_locations = [] 
 fna_files = [] # list of the fna files
 fna_file_location = [] #the location of the fna files
+sequence_data = [] #list of records
 i=0 
 j=0
 
@@ -29,17 +30,20 @@ while i< len(internal_directories):
 		fna_file_location.append(file_location)
 	i=i+1
 
-#while j < len(fna_file_location):
-#	for seq_record in SeqIO.parse(fna_file_location[j], "fasta"):
-#		print(seq_record.id)
-#		print(seq_record.description)
-		#print(repr(seq_record.seq))
-#		print(len(seq_record))
-
-while j < 1:
+while j < len(fna_file_location):
 	for seq_record in SeqIO.parse(fna_file_location[j], "fasta"):
-		fna_file =fna_files[j]
 		description = seq_record.description
-		#print(repr(seq_record.seq))
-		sequence_lentgth = len(seq_record)
+		match = re.search('Brucella\s\w+',description)
+		if match != None:
+			strain = match.group(0)
+		if match == None:
+			strain = "no strain found"	
+		record = [fna_files[j], seq_record.id, strain]
+		sequence_data.append(record)
 	j=j+1
+
+with open ('../Metadata.csv', 'w') as output:
+	output_writer = csv.writer(output, delimiter = '	')
+	output.write('File Name, Record ID, Strain\n')
+	for record in sequence_data:
+		output_writer.writerow(record)
