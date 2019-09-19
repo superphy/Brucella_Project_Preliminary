44cbefa980e2eaf8b0afff0b774638fff17e87af
ashlynns <ashlynn.steeves@icloud.com>
2019-09-19 15:27:51 -0600

    couple small swaps

--- source/quast.py
+++ source/quast.py
@@ -4,17 +4,16 @@ i=0
 metadata = pd.read_csv('Metadata.csv')
 fna_file_locations = metadata['File Location'].tolist()
 fna_file_names = metadata['File Name'].tolist()
-quast_file_names = []
 
 os.system("mkdir quast_output")
 
 while i< len(fna_file_locations):
-	file_name = "quast_"+fna_file_names[i]
-	quast_file_names.append([file_name])
 	sys_in = "python quast-5.0.2/quast.py -o quast_output/quast_"+fna_file_names[i]+" "+fna_file_locations[i]
 	os.system(sys_in)
+	if i == len(fna_file_locations)-1:
+		os.system("mkdir quast_output/quast_complete")
+		print("quast complete")
 	i = i+1
 
-df =pd.DataFrame.from_records(quast_file_names, columns =['File Name'])
-df.to_csv('Quast_Summary.csv', index=False)
+
 
