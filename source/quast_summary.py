6bfb0d828367be180cfcbff557f5a175a8e7c8f8
ashlynns <ashlynn.steeves@icloud.com>
2019-09-20 15:28:23 -0600

    modified output to simplify future steps

--- source/quast_summary.py
+++ source/quast_summary.py
@@ -1,7 +1,10 @@
 import os 
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 directories = (os.listdir("quast_output/"))
+contigs_list = []
+contigs_less_1000_list =[]
 quast_summary =[]
 i=0
 
@@ -12,11 +15,21 @@ while i < len(directories):
 		report = pd.read_csv(report_location, sep='\t')
 		data = report.iloc[:,1].to_list()
 		contigs = data[12]
+		contigs_list.append(contigs)
 		contigs_less_1000 = contigs - data[1]
-		row = [quast_file_name, contigs, contigs_less_1000]
+		contigs_less_1000_list.append(contigs_less_1000)
+		og_file_location = "refseq/bacteria/"+quast_file_name[6:21]+"/"+quast_file_name[6:len(quast_file_name)]
+		row = [og_file_location, quast_file_name, contigs, contigs_less_1000]
 		quast_summary.append(row)
 	i=i+1
 
-df =pd.DataFrame.from_records(quast_summary, columns =['File Name', 'Number of Contigs', 'Number of Contigs < 1000bp'])
+df =pd.DataFrame.from_records(quast_summary, columns =['Fna File Location','File Name', 'Number of Contigs', 'Number of Contigs < 1000bp'])
 df.to_csv('Quast_Summary.csv', index=False)
+
+scatterplot = plt.scatter(contigs_list, contigs_less_1000_list, s=10, c='k')
+plt.xlabel ("Number of Contigs")
+plt.ylabel("Number of Contigs <1000bp")
+ 
+plt.title('Data Quality Visualization')
+plt.savefig("Data Quality Visualization.png")
 
