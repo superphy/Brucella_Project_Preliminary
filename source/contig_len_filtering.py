746aea9c1c35e237401b5a9b40ff87b6e9735fe6
ashlynns <ashlynn.steeves@icloud.com>
2019-09-24 13:44:53 -0600

    changes to comply with kSNP naming standard

--- source/contig_len_filtering.py
+++ source/contig_len_filtering.py
@@ -9,6 +9,8 @@ files_w_contigs_under_1000bp =[]
 files_wo_contigs_under_1000bp =[]
 fna_names = []
 fna_file_locations = []
+file_location_list = []
+file_location_list_b = []
 
 while i< len(quast_summary):
 	quast_row = quast_summary.iloc[i]
@@ -22,10 +24,12 @@ while i< len(quast_summary):
 		files_wo_contigs_under_1000bp.append(file_info)	
 	i=i+1
 
-os.system("mkdir Approved_Sequences")
+#os.system("mkdir Approved_Sequences")
 
 for record in files_wo_contigs_under_1000bp:
 	cp = "cp "+record[0]+" Approved_Sequences/"
+	fll = "Approved_Sequences/"+record[1]
+	file_location_list.append(fll)
 	os.system(cp)
 
 for record in files_w_contigs_under_1000bp:
@@ -34,8 +38,16 @@ for record in files_w_contigs_under_1000bp:
 	for seq_record in SeqIO.parse(input_location, "fasta"):
 		if len(seq_record.seq)>500:
 			len_500_plus.append(seq_record)
-	output_location = "Approved_Sequences/"+record[1]		
+	output_location = "Approved_Sequences/"+record[1]
+	file_location_list.append(output_location)		
 	SeqIO.write(len_500_plus,output_location, "fasta")	
 
+original_files = (os.listdir("Approved_Sequences"))
 
+for file in original_files:
+	new_name = file[0:13]+"_"+file[14:len(file)]
+	sysin = "mv Approved_Sequences/"+file+" Approved_Sequences/"+new_name
+	os.system(sysin)
+
+print ("Done contig length filtering")	
 
