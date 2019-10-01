c8a6d45dc37a5822e380517bb114fcdc312a34d3
ashlynns <ashlynn.steeves@icloud.com>
2019-10-01 14:29:05 -0600

    name changes to comply with ksnp naming standards occur here

--- source/ksnp_input.py
+++ source/ksnp_input.py
@@ -1,18 +1,28 @@
 import os 
 import pandas as pd 
+i=0
+#IF YOU EDIT THIS FILE SNAKEMAKE WILL TRY TO RE-RUN KSNP
 
-files = os.listdir("Approved_Sequences/")
+original_files = os.listdir("Approved_Sequences/")
 input_list = []
 cwd = os.getcwd()
 
-
-for file in files:
-	file_location = cwd+"/"+file
-	id_name = file[4:13]
-	entry = file_location+"	"+id_name+'\n'
+for file in original_files:
+	new_name = file[0:len(file)-4]
+	newer_name = ""
+	#replacing all the periods with underscores
+	while i < len(new_name):
+		if new_name[i] == ".":
+			newer_name=new_name[0:i]+"_"+new_name[i+1:len(new_name)]
+			break
+		i=i+1
+	#renaming the actual files	
+	sysin = "mv Approved_Sequences/"+file+" Approved_Sequences/"+newer_name+".fna"
+	os.system(sysin)
+	entry = cwd+"/"+newer_name+".fna"+"	"+newer_name[4:13]+'\n'
 	input_list.append(entry)
 
+#writing output file
 with open("kSNP3_input.txt", "w") as output:
 	for row in input_list:
 		output.write(str(row))
-
