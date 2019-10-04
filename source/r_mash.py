a8576b4fcbdf45ce45c3019ecfdbbc765346d04a
ashlynns <ashlynn.steeves@icloud.com>
2019-10-04 11:48:12 -0600

    deletes all MD5SUMS files and runs refseq_masher

---
+++ source/r_mash.py
@@ -0,0 +1,22 @@
+import os 
+import pandas as pd
+
+files = os.listdir("refseq/bacteria")
+fna_files_rm = ""
+fna_files = []
+n = 3
+
+# Deletes all the MD5SUMS files
+for file in files:
+	sysin = "rm refseq/bacteria/"+file+"/MD5SUMS"
+	os.system(sysin)
+
+# gathers the name of all the fna files to be run in refseq_masher
+for file in files:
+	sysin = "refseq/bacteria/"+file
+	fna_files.append(os.listdir(sysin)[0])
+	fna_files_rm= fna_files_rm+" "+sysin+"/"+os.listdir(sysin)[0]
+
+#runs refseq_masher 
+sysin = "refseq_masher matches "+fna_files_rm+" -o rmash_output.csv /home/ashlynn/Desktop/Fall_2019/random --output-type csv -n "+str(n)
+os.system(sysin)
