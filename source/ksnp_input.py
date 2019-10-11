186cc49bea6a7c194290a4f2b6168666962cd8a0
ashlynns <ashlynn.steeves@icloud.com>
2019-10-11 11:52:44 -0600

    took out the renaming chunk of code as that is done when transfering files into approves sequences

--- source/ksnp_input.py
+++ source/ksnp_input.py
@@ -1,25 +1,14 @@
 import os 
 import pandas as pd 
-i=0
 #IF YOU EDIT THIS FILE SNAKEMAKE WILL TRY TO RE-RUN KSNP
 
 original_files = os.listdir("Approved_Sequences/")
 input_list = []
 cwd = os.getcwd()
 
-for file in original_files:
-	new_name = file[0:len(file)-4]
-	newer_name = ""
-	#replacing all the periods with underscores
-	while i < len(new_name):
-		if new_name[i] == ".":
-			newer_name=new_name[0:i]+"_"+new_name[i+1:len(new_name)]
-			break
-		i=i+1
-	#renaming the actual files	
-	sysin = "mv Approved_Sequences/"+file+" Approved_Sequences/"+newer_name+".fna"
-	os.system(sysin)
-	entry = cwd+"/"+newer_name+".fna"+"	"+newer_name[4:13]+'\n'
+#formatting
+for file in original_files: 
+	entry = cwd+"/Approved_Sequences/"+file+"	"+file[4:13]+'\n'
 	input_list.append(entry)
 
 #writing output file
