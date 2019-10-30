df30f4f3c515198eabafe3023f3c35992c5062ac
ashlynns <ashlynn.steeves@icloud.com>
2019-10-30 12:17:16 -0600

    determines the location of kmers of interest on a refrence genome

--- source/kmer_location.py
+++ source/kmer_location.py
@@ -5,21 +5,32 @@ from Bio import SeqIO
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
+pd.options.mode.use_inf_as_na = True
 
 metadata = pd.read_csv('../Metadata.csv')
 tkmers = pd.read_csv('../Top_kmers.csv', index_col = 0)
 
-species = tkmers.columns.values[1:len(tkmers.columns.values)]
-meta_ref = metadata[metadata['Number of Contigs'] == 2]
-meta_ref = meta_ref.drop_duplicates(subset = 'Species', keep='first')
+species = tkmers.columns.values[0:len(tkmers.columns.values)]
+meta_ref = metadata[metadata['Number of Contigs'] == 2] # only complete sequences (a.k.a 2 contigs)
+meta_ref = meta_ref.drop_duplicates(subset = 'Species', keep='first') # one of each species
+chroms = [' c1', ' c2']
+columns = [y+x for y in species for x in chroms]
 
+chrom1_dict = {}
+chrom2_dict = {}
+for kmer in tkmers.index:
+	chrom1_dict[kmer] = np.inf
+	chrom2_dict[kmer] = np.inf
+
+#Choosing and locating a refrence genome 
 def refrence_location(species):
 	ref_row = meta_ref[meta_ref['Species']==species]
 	ref_file = ref_row['Sample'].to_list()
 	ref_file_location = '../Approved_Sequences/'+ref_file[0]+'.fna'
 	return(ref_file_location)
 
-def refrence_file(species):
+#Creates 2 haystacks from the refrence genome, one for each chromosome
+def haystacks(species):
 	location = refrence_location(species)
 	sequence = []
 	with open(location) as handle:
@@ -29,27 +40,31 @@ def refrence_file(species):
 	chromosome2 = sequence[1]
 	return(chromosome1, chromosome2)
 
+#Creates the needles, which are the top X kmers identified for each species
 def needles(species):
-	kmer_df = tkmers[species].dropna(axis='index', how='all')
-	kmers = kmer_df.index.values
+	kmers = tkmers[species].dropna(axis='index', how='all').index.values
 	aho = ahocorasick.Automaton()
 	for i in range(0,len(kmers)):
-		aho.add_word(kmers[i], (i, kmers[i]))
+		aho.add_word(kmers[i],kmers[i])
 	aho.make_automaton()
 	return(aho)
 
+#Runs the ahocorasick algorithm to find the location of the kmers (needles) in the refrence genome (haystack) 
 def find_needles(species):
 	aho = needles(species)
-	print(aho)
-	#print(aho.get_stats())
-	#print(aho.get('ACGTAGCCCGATCGCTATTGCGCTTTTCATG'))
-	chromosome1, chromosome2 = refrence_file(species)
-	it = aho.iter(chromosome1)
-	print(it)
-	for index, value in it:
-		print(index, value) 
-	for item in aho.iter(chromosome1):
-		print("EXCUSE MEEEE")
+	chromosome1, chromosome2 = haystacks(species)
+	for end_index, kmer in aho.iter(chromosome1):
+		chrom1_dict[kmer] = end_index
+	for end_index, kmer in aho.iter(chromosome2):
+		chrom2_dict[kmer] = end_index
+	return(species, chrom1_dict, chrom2_dict)
 
+df = pd.DataFrame(columns = columns, index = tkmers.index.values)
 
-find_needles('Brucella sp')
+with ProcessPoolExecutor(len(species)) as ppe:
+	for species, chrom1_dict, chrom2_dict in ppe.map(find_needles, species):
+		df[species+' c1'] = chrom1_dict.values()
+		df[species+' c2'] = chrom2_dict.values()
+
+df.to_csv('Kmers_Locations.csv')
+print(df)
