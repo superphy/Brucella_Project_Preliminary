1dec5b00cfdb9b854fb1f3c50f3bd35c88cc145f
ashlynns <ashlynn.steeves@icloud.com>
2019-11-07 14:37:33 -0700

    matching metadata names and fixing up the kmer matrix

---
+++ source/kover_inputs.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+og_meta = pd.read_csv('../Metadata.csv', index_col = 0)
+strain_occ = pd.read_csv('../Strain_Occurances.csv', index_col = 0)
+kcounts = pd.read_csv('../kmer_counts.csv')
+
+index = []
+for name in og_meta.index:
+	index.append(name+'.fna')
+
+kover_meta = pd.DataFrame(index = index, columns = strain_occ.index, data=np.zeros((len(index),len(strain_occ.index))) )
+
+for i in range(0, len(og_meta.index)):
+	file = index[i]
+	species = og_meta['Species'].iloc[i]
+	kover_meta.loc[file,species]=1
+
+for i in range(0, len(strain_occ.index)):
+	strain = strain_occ.index[i]
+	kover_single_meta = kover_meta[strain]
+	under_strain = strain.replace(' ', '_')
+	kover_single_meta.to_csv('Kover_Data/Kover_'+under_strain+'_Metadata.tsv', sep='\t', header=False)
+
+kcounts.rename(columns={'Unnamed: 0':'kmers'}, inplace=True )
+kcounts.astype(bool)
+kcounts.to_csv('kmt.tsv', sep='\t', index=False)
