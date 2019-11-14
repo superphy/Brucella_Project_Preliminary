31ee3474de2e44e82bae54250a96ca24a8d4e4d1
Ashlynn <ashlynn.steeves@iicloud.com>
2019-11-14 13:30:05 -0700

    Determines the length of the longest genome for each species

--- source/longest.py
+++ source/longest.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import Bio 
+from Bio import SeqIO
 
 strain_occ = pd.read_csv('../Strain_Occurances.csv', index_col=0)
 species = strain_occ.index.values
@@ -8,7 +8,20 @@ metadata = pd.read_csv('../Metadata.csv', index_col=0)
 def longest(species):
 	species_meta = metadata[metadata['Species']==species]
 	samples = species_meta.index.values
+	longest_counter = 0
 	for i in range(0,len(species_meta)):
-		print(samples[i])
+		sample_len_counter = 0 
+		location = '../Approved_Sequences/'+samples[i]+'.fna'
+		for record in SeqIO.parse(location, 'fasta'):
+			sample_len_counter+=len(record.seq)
+		if sample_len_counter > longest_counter:
+			longest_counter = sample_len_counter
+	return(species, longest_counter)
 
-longest('Brucella suis')
+df = pd.DataFrame(index = species, columns = ['Longest Genome'], data=range(0,len(species)))
+
+for species in species:
+	species, longest_counter = longest(species)
+	df.at[species, 'Longest Genome' ] = longest_counter
+
+df.to_csv('../Longest_Genomes.csv')
