5ce2316d92277b52059af8092d1b9c729bfb4fe3
ashlynns <ashlynn.steeves@icloud.com>
2019-11-21 16:07:05 -0700

    updated for smk

--- source/kover_rules.py
+++ source/kover_rules.py
@@ -5,40 +5,38 @@ import numpy as np
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
 
-strain_occ = pd.read_csv('../Strain_Occurances.csv', index_col = 0)
+strain_occ = pd.read_csv('Strain_Occurances.csv', index_col = 0)
 strains = strain_occ.index.values.tolist()
 strains.remove('Brucella neotomae')
 strains.remove('Brucella sp')
 strains = [strain.replace(' ', '_') for strain in strains]
 
-def rules_parser(strain):
-	rule1 = '../Kover_Data/'+strain+'/model_rule_1_equiv.fasta'
-	rule2 = '../Kover_Data/'+strain+'/model_rule_2_equiv.fasta'
-	rules1, rules2 = [str() for c in 'c' * 10000], [str() for c in 'c' * 10000]
-	header1, header2 = '', ''
-	i1, i2 = 0,0
-	if os.path.isfile(rule1):
-		for record in SeqIO.parse(rule1, 'fasta'):
+def rule_reader (rule): #Reads the fasta file of the given rule (if it exists) and returns a list of all the sequences and what the rule is
+	rules = [str() for c in 'c' * 10000] #Create a list of empty 10000 strings (the number of possible rules)
+	header = ''
+	i = 0 
+	if os.path.isfile(rule): #If the rule exists exists
+		for record in SeqIO.parse(rule, 'fasta'):
 			seq = record.seq
 			seq = seq._get_seq_str_and_check_alphabet(seq)
-			rules1[i1]=seq
-			header1 = record.id
-			i1+=1 
-	if os.path.isfile(rule2):
-		for record in SeqIO.parse(rule2, 'fasta'):
-			seq = record.seq
-			seq = seq._get_seq_str_and_check_alphabet(seq)
-			rules2[i2]=seq
-			header2 = record.id
-			i2+=1
+			rules[i]=seq #Place the sequence into the appropriate location of the list of empty strings
+			header = record.id 
+			i+=1 
+	return(header, rules)
+
+def rules_merger(strain): #Calls Rule reader for all the possible rules
+	rule1 = 'Kover_Data/'+strain+'/model_rule_1_equiv.fasta'
+	rule2 = 'Kover_Data/'+strain+'/model_rule_2_equiv.fasta'
+	header1, rules1 = rule_reader(rule1)
+	header2, rules2 = rule_reader(rule2)
 	return(strain, header1, rules1, header2, rules2)
 
 df = pd.DataFrame()
 
 with ProcessPoolExecutor(len(strains)) as ppe:
-	for strain, header1, rules1, header2, rules2 in ppe.map(rules_parser, strains):
+	for strain, header1, rules1, header2, rules2 in ppe.map(rules_merger, strains):
 		df[strain+'-'+header1]=rules1
 		if header2 != '':
 			df[strain+'-'+header2]=rules2
 
-df.to_csv('../Kover_Rules.csv', index = False)
+df.to_csv('Kover_Rules.csv', index = False)
