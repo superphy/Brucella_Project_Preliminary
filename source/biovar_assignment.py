8844cfe7b4dfcd8273bd4e7fe3b5e6321933ff49
ashlynns <ashlynn.steeves@icloud.com>
2019-11-29 14:37:55 -0700

    determines biovar classification based on amplicon size of various primers

--- source/biovar_assignment.py
+++ source/biovar_assignment.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-bpm = pd.read_csv('../Blast_Primer_Matching.csv')
+bpm = pd.read_csv('Blast_Primer_Matching.csv')
 files = list(bpm['File ID'].unique())
 
 biovar_1 = [773, 774, 775, 424, 425, 426, 196, 197, 198]
@@ -11,15 +11,16 @@ biovar_5 = [773, 613, 277, 196]
 
 def biovar(file):
 	file_df = bpm[bpm['File ID']==file].fillna(0)
-	p1 = file_df['Primer One (BMEI1426-BMEI1427)'].to_list()
-	p2 = file_df['Primer Two (BR1080f-BR1080r)'].to_list()
-	p3 = file_df['Primer Three (BMEI1688-BMEI1687)'].to_list()
-	p4 = file_df['Primer Four (BMEI0205f-BMEI0205r)'].to_list()
+	p1 = file_df['Primer One (BMEI1426-BMEI1427)'].to_list() # amplicons derived from primer 1
+	p2 = file_df['Primer Two (BR1080f-BR1080r)'].to_list() # amplicons derived from primer 2
+	p3 = file_df['Primer Three (BMEI1688-BMEI1687)'].to_list() # amplicons derived from primer 3
+	p4 = file_df['Primer Four (BMEI0205f-BMEI0205r)'].to_list() # amplicons derived from primer 4
 
 	amplicons = p1+p2+p3+p4
-	amplicons = [int(amp) for amp in amplicons if amp != 0]
+	amplicons = [int(amp) for amp in amplicons if amp != 0] # removing the nan values 
+	amplicons = list(set(amplicons))
 
-	if len(amplicons)<3:
+	if len(amplicons)<3: # every biovar class requires at least three primer matches, therefore if there are less then three amplicons
 		return('No Matched Biovar')
 	else:
 		bv1 = all(amps in biovar_1 for amps in amplicons)
@@ -28,22 +29,22 @@ def biovar(file):
 		bv4 = all(amps in biovar_4 for amps in amplicons)
 		bv5 = all(amps in amplicons for amps in biovar_5)
 
-		if bv1:
+		if bv1: # if all of the amplicons found in the given file are found in the biovar 1 criteria
 			return('Biovar 1')
-		if bv2:
+		if bv2: # if all of the amplicons returned for one sample are found in the biovar 2 criteria
 			return('Biovar 2')
-		if bv3:
+		if bv3: # if all of the amplicons returned for one sample are found in the biovar 3 criteria
 			return('Biovar 3')
-		if bv5:
+		if bv5: # if all of the amplicons returned for one sample are found in the biovar 5 criteria
 			return('Biovar 5')
-		if bv4:
+		if bv4: # if all of the amplicons returned for one sample are found in the biovar 4 criteria
 			return('Biovar 4')
-		else:
+		else: 
 			return('No Matched Biovar')
 
-df = pd.DataFrame()
+df = pd.DataFrame(index = ['Biovar Classification'])
 for file in files:
 	df[file] = [biovar(file)]
 
 df = df.T
-df.to_csv('../Suis_Biovar.csv')
+df.to_csv('Suis_Biovar.csv')
