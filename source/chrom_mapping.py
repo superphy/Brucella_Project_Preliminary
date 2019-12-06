6e6aa0e16a0e5ca683d19c6885e2c3f4d1de3051
ashlynns <ashlynn.steeves@icloud.com>
2019-12-06 14:20:21 -0700

    evaluating negative rank values

--- source/chrom_mapping.py
+++ source/chrom_mapping.py
@@ -8,14 +8,17 @@ from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
 pd.options.mode.use_inf_as_na = True
 
-metadata = pd.read_csv('Metadata.csv')
-ranks = pd.read_csv('Ranks.csv', index_col = 0)
-strain_occurances = pd.read_csv("Strain_Occurances.csv", index_col = 0)
-species = strain_occurances.index.values 
+metadata = pd.read_csv('../Metadata.csv')
+ranks = pd.read_csv('../Ranks.csv', index_col = 0)
+ranks = ranks.filter(axis = 1, like='RANK')
+strain_occurances = pd.read_csv("../Strain_Occurances.csv", index_col = 0)
+strain = strain_occurances.index.values 
 col_dict = {'Brucella sp':'#ebc04b', 'Brucella suis':'#f8a4d0','Brucella abortus': '#72a24a','Brucella canis': '#b8642f','Brucella ceti':'#59cf9e', 'Brucella melitensis':'#e8382e', 'Brucella neotomae':'#6c86e2', 'Brucella ovis':'#ba6eb4', 'Brucella pinnipedialis':'#f4792a' }
 meta_ref = metadata[metadata['Number of Contigs'] == 2] # only complete sequences (a.k.a 2 contigs)
-meta_ref = meta_ref.drop_duplicates(subset = 'Species', keep='first') # one of each species
+meta_ref = meta_ref.drop_duplicates(subset = 'Species', keep='first') # one of each strain
+meta_ref.to_csv('../Refrence_Genomes.csv')
 complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+alt_ref_strain = {}
 
 c1_dict, c2_dict = {}, {}
 for kmer in ranks.index: 
@@ -23,31 +26,43 @@ for kmer in ranks.index:
 	c2_dict[kmer] = np.inf
 
 #Choosing and locating a refrence genome 
-def refrence_location(species):
-	ref_row = meta_ref[meta_ref['Species']==species]
-	ref_file = ref_row['Sample'].to_list()
-	ref_file_location = 'Approved_Sequences/'+ref_file[0]+'.fna'
-	return(ref_file_location)
+def refrence_location(strain):
+	ref_row = meta_ref[meta_ref['Species']==strain]
+	ref_file = list(ref_row['Sample'])
+	ref_file_location = '../Approved_Sequences/'+ref_file[0]+'.fna'
+	return(ref_file_location, ref_file[0])
+
+def alt_ref(strain):
+	sort_rank = ranks.sort_values(by = [strain+" RANK"]).head(1)
+	kmer = list(sort_rank.index.values)
+	sort_rank = sort_rank.T.sort_values(by = kmer, ascending=False).head(1)
+	alt_st = sort_rank.index.values[0][0:len(sort_rank.index.values[0])-5]
+	alt_ref_strain[strain]=alt_st
+	alt_ref_file_location, ref_file = refrence_location(alt_st)
+	return(alt_ref_file_location, ref_file)
 
 #Creates 2 haystacks from the refrence genome, one for each chromosome
-def haystacks(species):
-	location = refrence_location(species)
+def haystacks(strain, soa):
+	if soa == 'self':
+		location, ref_file= refrence_location(strain)
+	if soa == 'alt':
+		location, ref_file = alt_ref(strain)
 	sequence = []
 	with open(location) as handle:
 		for title, seq in SimpleFastaParser(handle):
 			sequence.append(seq)
 	chromosome1 = sequence[0]
 	chromosome2 = sequence[1]
-	return(chromosome1, chromosome2)
+	return(chromosome1, chromosome2, ref_file)
 
 #Generates the reverse complement of a given kmer
 def reverse_complement(kmer):
 	return ''.join([complement[base] for base in kmer[::-1]])
 
 #Creates the needles (all kmers)
-def needles(species):
+def needles(strain):
 	kmers = ranks.index.values
-	rank = ranks[species+' RANK']
+	rank = ranks[strain+' RANK']
 	aho = ahocorasick.Automaton()
 	for i in range(0,len(kmers)):
 		kmer = kmers[i]
@@ -58,9 +73,10 @@ def needles(species):
 	return(aho)
 
 #Runs the ahocorasick algorithm to find the location of the kmers (needles) in the refrence genome (haystack) 
-def find_needles(species):
-	aho = needles(species)
-	chromosome1, chromosome2 = haystacks(species)
+def find_needles(tup):
+	soa, strain = tup
+	aho = needles(strain)
+	chromosome1, chromosome2, ref_file = haystacks(strain, soa)
 	c1_index, c1_rank, c2_index, c2_rank = [], [], [], []
 	for end_index, (kmer, rank) in aho.iter(chromosome1):
 		c1_index.append(end_index)
@@ -70,12 +86,14 @@ def find_needles(species):
 		c2_index.append(end_index)
 		c2_dict[kmer] = end_index
 		c2_rank.append(rank)
-	return(species, c1_index, c1_rank, c2_index, c2_rank, c1_dict, c2_dict)
+	return(soa, strain, ref_file, c1_index, c1_rank, c2_index, c2_rank, c1_dict, c2_dict)
 
 df = pd.DataFrame(index = ranks.index)
+in_list = [(x,y) for y in strain for x in ['self', 'alt']]
 
-with ProcessPoolExecutor(len(species)) as ppe:
-	for species, c1_index, c1_rank, c2_index, c2_rank, c1_dict, c2_dict in ppe.map(find_needles, species):
+
+with ProcessPoolExecutor(len(strain)) as ppe:
+	for soa, strain, ref_file, ref_strain, c1_index, c1_rank, c2_index, c2_rank, c1_dict, c2_dict in ppe.map(find_needles, in_list):
 		#For any kmer that is not found in the refrence genome it is assigned a value of zero
 		c1_index_padded = range(0, max(c1_index))
 		c1_rank_padded = np.zeros(max(c1_index))
@@ -91,9 +109,10 @@ with ProcessPoolExecutor(len(species)) as ppe:
 			index = c2_index[i]
 			c2_rank_padded[index-1]=c2_rank[i]
 		
+
 		#Creating the dataframe of kmer locations
-		df['C1 '+species] = c1_dict.values()
-		df['C2 '+species] = c2_dict.values()
+		df['C1 '+strain+' '+soa] = c1_dict.values()
+		df['C2 '+strain+' '+soa] = c2_dict.values()
 
 		#Generating the moving average of the rank values
 		df_c1_rank = pd.DataFrame(c1_rank_padded)
@@ -103,18 +122,19 @@ with ProcessPoolExecutor(len(species)) as ppe:
 
 		#Generating plots
 		fig, (ax1, ax2) = plt.subplots(1,2, sharey = True, figsize=(17,7))
-		fig.suptitle(species)
-		
+		fig.suptitle(strain+" "+soa+'\n'+ref_file)
+			
 		ax1.plot(c1_index_padded, c1_rank_padded, color= '#dedcdf', linewidth=0.25, ls = ':')
-		ax1.plot(c1_index_padded, roll_c1, color=col_dict[species], linewidth=1)
+		ax1.plot(c1_index_padded, roll_c1, color=col_dict[strain], linewidth=1)
 		ax1.set_ylim([-1,1])
 		ax1.set_title('Chromosome 1')
-		
+			
 		ax2.plot(c2_index_padded, c2_rank_padded, color='#dedcdf', linewidth=0.25, ls = ':')
-		ax2.plot(c2_index_padded, roll_c2, color=col_dict[species], linewidth=1)
+		ax2.plot(c2_index_padded, roll_c2, color=col_dict[strain], linewidth=1)
 		ax2.set_ylim([-1,1])
 		ax2.set_title('Chromosome 2')
 		
-		plt.savefig('Kmer_Location_Visualizations/'+species+'.png')
-
-df.to_csv('Kmer_Locations.csv')
+		name =strain+'_'+soa+'.png'.replace(' ', '_')
+		plt.savefig('../Kmer_Location_Visualizations/'+name)
+				
+df.to_csv('../Kmer_Locations.csv')
--- source/top_200bp.py
+++ source/top_200bp.py
@@ -5,79 +5,109 @@ from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
 pd.options.mode.use_inf_as_na = True
 
-ranks = pd.read_csv('../Ranks.csv', index_col = 0)
-k_loc = pd.read_csv('../Kmer_Locations.csv', index_col = 0)
+ranks_df = pd.read_csv('../Ranks.csv', index_col = 0)
+ranks_df = ranks_df.filter(axis = 1, like='RANK')
+ranks_index = ranks_df.index.values
+refrence_genomes = pd.read_csv('../Refrence_Genomes.csv')
+k_loc = pd.read_csv('Kmer_Locations_test.csv', index_col = 0)
 strain_occ = pd.read_csv('../Strain_Occurances.csv', index_col = 0)
 strains = strain_occ.index.values
 col_dict = {'Brucella sp':'#ebc04b', 'Brucella suis':'#f8a4d0','Brucella abortus': '#72a24a','Brucella canis': '#b8642f','Brucella ceti':'#59cf9e', 'Brucella melitensis':'#e8382e', 'Brucella neotomae':'#6c86e2', 'Brucella ovis':'#ba6eb4', 'Brucella pinnipedialis':'#f4792a' }
 chroms = {'C1':'C2', 'C2':'C1'}
+in_list = [(x,y) for y in strains for x in ['self', 'alt']]
 
-def rolling(rank_df):
+def rolling(rank_df): # takes the rolling average of the rank values sorted by location
 	roll_mean = rank_df.rolling(window=200).mean()
 	return(list(roll_mean))
 
-def strain_df_f(strain):
-	strain_df = pd.DataFrame(index = ranks.index.values)
-	strain_df['Rank'] = ranks[strain+' RANK']
-	strain_df['C1 Loc'] = k_loc['C1 '+strain]
-	strain_df['C2 Loc'] = k_loc['C2 '+strain]
-	strain_df = strain_df.dropna(axis = 0, thresh=2)
-	return(strain_df)
+def strain_df_f(strain, soa): # returns a strain specific df with the rank of each kmer and their location on either chromosome
+	strain_df = pd.DataFrame(index = ranks_index)
+	if soa == 'self':
+		strain_df['Rank'] = ranks_df[strain+' RANK']
+		strain_df['C1 Loc'] = k_loc['C1 '+strain+' self']
+		strain_df['C2 Loc'] = k_loc['C2 '+strain+' self']
+		strain_df = strain_df.dropna(axis = 0, thresh=2) # drop any rows where the kmer isnt found on any chromosome
+		return(strain_df)
+	if soa == 'alt':
+		strain_df['Rank'] = ranks_df[strain+' RANK']
+		strain_df['C1 Loc'] = k_loc['C1 '+strain+' alt']
+		strain_df['C2 Loc'] = k_loc['C2 '+strain+' alt']	
+		strain_df = strain_df.dropna(axis = 0, thresh=2) # drop any rows where the kmer isnt found on any chromosome
+		return(strain_df)
 
-def loc_dict_f(strain_df):
+def loc_dict_f(strain_df): # generates an empty dictionary to set rank values for locations without a match to zero
 	loc_dict = {}
 	locs = list(strain_df['C1 Loc'].dropna())+list(strain_df['C2 Loc'].dropna())
-	for i in range(0, int(max(locs))):
+	for i in range(0, int(max(locs))): # zero to the largest number found as a location on either C1 or C2 
 		loc_dict[i] = (0, np.inf)
 	return(loc_dict)
 
-def top_row(strain, chrom):
-	strain_df= strain_df_f(strain)
+def top_row(strain, chrom, soa):
+	strain_df= strain_df_f(strain, soa)
+	print(strain, chrom, soa, '\n')
 	loc_dict = loc_dict_f(strain_df)
 	 
 	strain_df = strain_df.drop(columns=chroms[chrom]+' Loc').dropna(axis =0, how='any')
 	for index, row in strain_df.iterrows():
-		loc_dict[row[chrom+' Loc']]=(row['Rank'], row.name)
+		loc_dict[row[chrom+' Loc']]=(row['Rank'], row.name) # key = location, value = (rank, kmer)
 	df = pd.DataFrame()
 	
-	kmers = [value[1] for value in loc_dict.values()]
-	ranks = [value[0] for value in loc_dict.values()]
+	kmers = [value[1] for value in loc_dict.values()] # pulling the kmers from the dict
+	ranks = [value[0] for value in loc_dict.values()] # pulling the rank values from the dict
 	
-	df['Location'] = loc_dict.keys()
-	df['Kmer'] = kmers
-	df['Ranks'] = ranks
+	df['Location'] = loc_dict.keys() # adding location column to df 
+	df['Kmer'] = kmers # adding kmer column to df 
+	df['Ranks'] = ranks # adding ranks column to df 
 
 	roll_mean= rolling(df['Ranks'])
-	df['Rolling Avg'] = roll_mean
-	df = df.sort_values(by=['Rolling Avg'], ascending=False).head(1)
+	df['Rolling Avg'] = roll_mean # adding rolling average column to df 
+	df = df.sort_values(by=['Rolling Avg'], ascending=False).head(1) # returns the row in the df with the largest rolling average value
+
 	return(df, loc_dict)
 
-def intermediate(strain):
-	c1_top_row, c1_loc_dict = top_row(strain, 'C1')
-	c2_top_row, c2_loc_dict = top_row(strain, 'C2')
+#top_row('Brucella abortus','C1', 'self')
+#top_row('Brucella suis','C2', 'alt')
 
-	max_ra = max(float(c1_top_row['Rolling Avg']), float(c2_top_row['Rolling Avg']))
-	
-	if max_ra == float(c1_top_row['Rolling Avg']):
-		out_col = [c1_top_row['Kmer'].to_string(index = False), 'C1',int(c1_top_row['Location'])-200, int(c1_top_row['Location']), float(c1_top_row['Ranks']), float(c1_top_row['Rolling Avg'])]
-		locations = range(int(c1_top_row['Location'])-200, int(c1_top_row['Location']))
-		ranks = [c1_loc_dict[loc] for loc in locations]
-		return(strain, out_col, locations, ranks)
-	
-	if max_ra == float(c2_top_row['Rolling Avg']):
-		out_col = [c2_top_row['Kmer'].to_string(index = False), 'C2', int(c2_top_row['Location'])-200, int(c2_top_row['Location']), float(c2_top_row['Ranks']), float(c2_top_row['Rolling Avg'])]
-		locations = range(int(c1_top_row['Location'])-200, int(c1_top_row['Location']))
-		ranks = [c1_loc_dict[loc] for loc in locations]
-		return(strain, out_col, locations, ranks)
+def intermediate(in_list):
+	soa, strain = in_list
+	c1_top_row, c1_loc_dict = top_row(strain, 'C1', soa)
+	c2_top_row, c2_loc_dict = top_row(strain, 'C2', soa)
 
-final_df = pd.DataFrame(index = ['Kmer', 'Chromosome','Start Location', 'End Location', 'Rank', 'Rolling Avg'])
+	ref_genome = refrence_genomes[refrence_genomes['Species']==strain]
+	ref_genome = list(ref_genome['Sample'])
+	ref_genome = ref_genome[0]
+
+	max_ra = max(float(c1_top_row['Rolling Avg']), float(c2_top_row['Rolling Avg'])) # is the rolling average on C1 or C2 larger? 
+	
+	if max_ra == float(c1_top_row['Rolling Avg']): # if C1 is larger
+		# the list returned to be the col in the df [chrom. start loc, end loc, rank, rolling avg, ref genome]
+		out_col = ['C1',int(c1_top_row['Location'])-200, int(c1_top_row['Location']), float(c1_top_row['Ranks']), float(c1_top_row['Rolling Avg']), ref_genome]
+		# lists used to plot the rank values from the start loc to the end loc of the top 200 bp
+		locations = [i for i in range(int(c1_top_row['Location'])-200, int(c1_top_row['Location']))]
+		ranks = [c1_loc_dict[loc][0] for loc in locations]
+		return(strain, out_col, locations, ranks, 'C1', ref_genome, soa)
+	
+	if max_ra == float(c2_top_row['Rolling Avg']): #If C2 is larger
+		# the list returned to be the col in the df [chrom. start loc, end loc, rank, rolling avg, ref genome]
+		out_col = ['C2', int(c2_top_row['Location'])-200, int(c2_top_row['Location']), float(c2_top_row['Ranks']), float(c2_top_row['Rolling Avg']), ref_genome]
+		# lists used to plot the rank values from the start loc to the end loc of the top 200 bp
+		locations =[i for i in range(int(c2_top_row['Location'])-200, int(c2_top_row['Location']))]
+		ranks = [c2_loc_dict[loc][0] for loc in locations]
+		return(strain, out_col, locations, ranks, 'C2', ref_genome, soa)
+final_df = pd.DataFrame(index = ['Chromosome','Start Location', 'End Location', 'Rank', 'Rolling Avg', 'Refrence Genome'])
+
 with ProcessPoolExecutor(cpu_count()-10) as ppe:
-	for strain, column, locations, ranks in ppe.map(intermediate, strains):
-		final_df[strain] = column
-		plt.plot(ranks, locations, color=col_dict[strain])
+	for strain, column, locations, ranks, chrom, ref_genome, soa in ppe.map(intermediate, in_list):
+		final_df[strain+" "+soa] = column # adding a column to the df for the given species
+
+		# generating figure to show the rank values across the top 200 bp
+		plt.figure(figsize = (17,7))
+		plt.plot(locations, ranks, color=col_dict[strain])
 		plt.xlabel('Location')
 		plt.ylabel('Rank')
-		plt.title('Rank Distribution for Top 200 bp of '+strain)
-		plt.show()
+		plt.title('Rank Distribution for Top 200 bp on '+chrom+' of '+strain+' '+soa+'\n'+ref_genome)
+
+		plt.savefig('../Kmer_Location_Visualizations/Top_200/'+strain+'_'+soa+'.png')
+		plt.clf()
 
 final_df.to_csv('Top_200_bp.csv')
